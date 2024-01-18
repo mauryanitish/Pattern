225f668d92b1ee32854646cc630918ef7e471add
mauryanitish <m.nitishkumar786@gmail.com>
2024-01-18 21:57:56 +0530

    complete Alphabet pattern

--- Alphabet/Q.java
+++ Alphabet/Q.java
@@ -1,14 +1,14 @@
-public class P {
+public class Q {
     public static void main(String[] args) {
         int n =11;
-        for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                if (i==0||j==0||i==n-1||j==n-1){
+
+        for (int i = 0;i<n;i++){
+            for (int j =0;j<n;j++){
+                if (i==0 ||j==0||i==n-1||j==n-1){
                     System.out.print("$ ");
-                }else if ((j==2&&i>2&&i<=n-3) || (i==2||i==n/2)&&(j>2&&j<=n-4) ||(j==n-3&&i>=3&&i<n/2)){
+                }else if (((i==2||i==n-3)&&(j>=3&&j<=n-4)) || ((j==2||j==n-3)&&(i>=3&&i<=n-4)) || (i==j&&i>n/2)){
                     System.out.print("* ");
-                }
-                else {
+                }else {
                     System.out.print("  ");
                 }
             }
--- Alphabet/S.java
+++ Alphabet/S.java
@@ -1,14 +1,13 @@
-public class P {
+public class S {
     public static void main(String[] args) {
         int n =11;
-        for (int i=0;i<n;i++){
+        for (int i =0;i<n;i++){
             for (int j=0;j<n;j++){
                 if (i==0||j==0||i==n-1||j==n-1){
                     System.out.print("$ ");
-                }else if ((j==2&&i>2&&i<=n-3) || (i==2||i==n/2)&&(j>2&&j<=n-4) ||(j==n-3&&i>=3&&i<n/2)){
+                }else if (((i==2||i==n/2||i==n-3)&&(j>2&&j<n-3))||(j==2&&i>2&&i<n/2)||(j==n-3&&i>n/2&&i<n-3)){
                     System.out.print("* ");
-                }
-                else {
+                }else {
                     System.out.print("  ");
                 }
             }
